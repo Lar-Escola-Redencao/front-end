6ad9ebd53681a5cca1029593a3eb4125eb2343a1
Gabrielle Ulisses <gabrielle.ulisses@aluno.ifsp.edu.br>
2026-08-21 00:26:26 -0300

    fix: ajusta proxy da api e corrige redirecionamento de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MembroComponent } from './components/membro/membro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'membros', component: MembroComponent },
+  { path: '', redirectTo: '/membros', pathMatch: 'full' }
+];
