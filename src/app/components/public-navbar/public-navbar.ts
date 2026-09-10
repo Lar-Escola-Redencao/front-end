619ca3f8aa58be4d7cfd5e817a5ae061c5e01ace
gabrielalb5 <gabrielalbino28@gmail.com>
2026-09-09 23:12:22 -0300

    feat: página base "conheça a osc"

--- src/app/components/public-navbar/public-navbar.ts
+++ src/app/components/public-navbar/public-navbar.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-public-navbar',
   standalone: true, 
-  imports: [MatButtonModule], 
+  imports: [MatButtonModule, RouterLink], 
   templateUrl: './public-navbar.html',
   styleUrl: './public-navbar.css',
 })
@@ -18,15 +18,21 @@ export class PublicNavbar {
     this.menuAberto.update(valor => !valor);
   }
 
+  fecharMenu() {
+    this.menuAberto.set(false);
+  }
+
   realizarLogin() {
     this.menuAberto.set(false);
     this.router.navigate(['/entrar']);
   }
 
-  scrollTo(sectionId: string) {
+  navegarParaHomeSection(sectionId: string) {
     this.menuAberto.set(false);
-    
-    if (this.router.url !== '/') {
+
+    const rotaAtual = this.router.url.split('?')[0].split('#')[0];
+
+    if (rotaAtual !== '/') {
       this.router.navigate(['/']).then(() => {
         setTimeout(() => this.executarScroll(sectionId), 100);
       });
